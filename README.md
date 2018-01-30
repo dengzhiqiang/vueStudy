89282741368ea0d965d41759ecd90d8292d7e1dc
dengzhiqiang <1445124133@qq.com>
2018-01-30 17:13:07 +0800

    'vue'

--- README.md
+++ README.md
@@ -13,5 +13,13 @@
     
     
     
+### 安装脚手架
+    https://www.jianshu.com/p/1626b8643676  脚手架目录介绍
     
-    
+    npm install vue-cli -g  
+    vue -V（注意这里是大写的“V”） 如果出现相应的版本号，则说明安装成功。
+    vue init webpack 项目名称     
+    cd 项目  进入项目名称
+    安装项目依赖：npm install
+    安装 vue 路由模块 vue-router 和网络请求模块 vue-resource，输入：cnpm install vue-router vue-resource --save
+    启动项目，输入：npm run dev
--- zhinengshe/3/vue-loader-demo/webpack.config.js
+++ zhinengshe/3/vue-loader-demo/webpack.config.js
@@ -1,19 +1,25 @@
-module.exports={
-	entry:'./main.js',
+module.exports = {
+    entry: './main.js',
 
-	output:{
-		path:__dirname,
-		filename:'build.js'
-	},
+    output: {
+        path: __dirname,
+        filename: 'build.js'
+    },
 
-	module:{
-		loaders:[
-			{test:/\.vue$/, loader:'vue'},
-			{test:/\.js$/, loader:'babel', exclude:/node_modules/}
-		]
-	},
-	babel:{
-		presets:['es2015'],
-		plugins:['transform-runtime']
-	}
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
 };
